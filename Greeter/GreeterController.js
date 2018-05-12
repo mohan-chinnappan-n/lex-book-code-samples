2f7cbeaacc480679f99db8806f635c9c333fd3f5
Mohan Chinnappan <mohan.chinnappan.n@gmail.com>
2018-05-12 14:50:58 -0400

    init

--- Greeter/GreeterController.js
+++ Greeter/GreeterController.js
@@ -1,21 +1,25 @@
-// Component Controller 
+// Component Controller
 //  it can have handlers
-//  
+//
 //  Controller can make use of functions in the Helper
-//  
+//
 //  Helper contains reusable methods the Controller can make use of
-//  
+//
 
 ({
 
 
     // onclick handler for the button
     // brings in 'component' and 'helper'
-    changeGreeting : function(component,event,helper) {
-        // set this component variable (v.greeting) to the 'newGreeting'  
-        //  with the help of the helper function : updateGreeting(component)    
+    changeGreeting : function(component, event, helper) {
+        // set this component variable (v.greeting) to the 'newGreeting'
+        //  with the help of the helper function : updateGreeting(component)
         helper.updateGreeting(component);
 
-    }
+    },
+    //onselect handler for the buttonMenu
+    handleLangMenuSelect: function(component, event, helper) {
+     helper.updateGreetingGivenLang(component, event);
 
+   }
 })
